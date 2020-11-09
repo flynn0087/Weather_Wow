e48d20d4eedbf8047f6e584293e6c4adf95daaf7
Ben Flynn <flynn0087@gmail.com>
2020-11-08 20:05:35 -0600

    call function for api

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
 let apiKey = "4bfa68a6f9552c990302f5d6025c306a";
 let city = $("#citySearch").val();
 
+$("#searchBtn").on("click", function() {
+    city = $("#citySearch").val();
+    let queryUrl = "https://api.openweathermap.org/data/2.5/onecall?" + city + apiKey;
+    
+})

7ffdd948cc9dd5f68d928930556ed642ff7ef1b5
Ben Flynn <flynn0087@gmail.com>
2020-11-08 20:41:24 -0600

    functioning API call to console

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
-let apiKey = "4bfa68a6f9552c990302f5d6025c306a";
+let apiKey = "&appid=4bfa68a6f9552c990302f5d6025c306a";
 let city = $("#citySearch").val();
 
 $("#searchBtn").on("click", function() {
-    city = $("#citySearch").val();
-    let queryUrl = "https://api.openweathermap.org/data/2.5/onecall?" + city + apiKey;
-    
+    let city = $("#citySearch").val();
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    .then(function (response){
+        console.log(response);
+    })
+    //todaysTemp(response);
+    //forecastTemp(response);
 })
